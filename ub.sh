293f8bc7e1ae6e745079d303040a8577fca0d88c
Jelly27 <mua_42@163.com>
2023-11-10 02:52:01 -0600

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # 下载安装miniconda
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# wget https://mirrors.bfsu.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
 # bash Miniconda3-latest-Linux-x86_64.sh -b
 # source miniconda3/bin/activate
 # conda init
